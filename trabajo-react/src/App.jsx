70172ee3db269455c163cc97c2dc3484461530a8
edithmariacaroortiz <edithmariacaroortiz809@gmail.com>
2026-05-31 18:16:59 -0500

    Primer proyecto React

---
+++ trabajo-react/src/App.jsx
@@ -0,0 +1,12 @@
+import TarjetaPersonal from "./TarjetaPersonal";
+
+function App() {
+  return (
+    <div>
+      <h1>Perfil de Edith María</h1>
+      <TarjetaPersonal />
+    </div>
+  );
+}
+
+export default App;
---
+++ trabajo-react/src/TarjetaPersonal.jsx
@@ -0,0 +1,19 @@
+function TarjetaPersonal() {
+  return (
+    <div className="tarjeta">
+      <img
+        src="https://i.pravatar.cc/200"
+        alt="Foto de perfil"
+      />
+
+      <h2>Edith María</h2>
+      <p>Edad: 18 años</p>
+      <p>Carrera: Desarrollo de Software</p>
+      <p>Color favorito: (verde)</p>
+
+      <button>Contacto</button>
+    </div>
+  );
+}
+
+export default TarjetaPersonal;
